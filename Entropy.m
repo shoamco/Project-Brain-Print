28ac1bba687ed27b74572b4820f0e97722ff253e
shoamco <shoamc98@gmail.com>
2018-01-09 11:47:28 +0200

    Data extraction and activation of entropy on the waves (Matlab)

--- Entropy.m
+++ Entropy.m
@@ -29,83 +29,15 @@ for i=1:numRaws
         entropy=entropy1+entropy2;%
         EntropyBetween2waves(i,j)=entropy;
   
-%         for k=1:numRaws
-%             wave3=  M(k,2:end);
-%             entropy3 = wentropy(wave3,entropyType);%%calculate Entropy Between 3 waves
-%             entropy=entropy1+entropy2+entropy3;%
-%             EntropyBetween3waves(i,j,k)=entropy;
-%         end
-    end
-end
-SubEntropy=[];
-arrMin=[];
-arrMin2=[];
-arrMax=[];
-for i=1:numRaws
-    e1=EntropyBetween2waves(i,i);
-    for j=1:numRaws
-        if i>j
-              e2=EntropyBetween2waves(i,j);
-              SubEntropy(j,i)=abs(abs(e1)-abs(e2));
+        for k=1:numRaws
+            wave3=  M(k,2:end);
+            entropy3 = wentropy(wave3,entropyType);%%calculate Entropy Between 3 waves
+            entropy=entropy1+entropy2+entropy3;%
+            EntropyBetween3waves(i,j,k)=entropy;
         end
     end
 end
-for i=1:numRaws
-     k=1;    
-     flag=0;
-     arr=SubEntropy(:,i)
-      while k<numRaws && flag==0
-          
-            valMin=min(arr)
-            if valMin~=10000
-                t='yes'
-              
-    %             valMax=max(arr)
-                 indexMin=find(arr==valMin);
-    %             indexMax=find(arr==valMax)
 
-    %             arrMin(i)=indexMin;
-
-                 arrMin2(i,k)=indexMin;
-    %             arrMax(k,i)=find(arr==valMax);
-                
-                arr(indexMin)=10000;
-                k=k+1
-                
-            
-            else
-                flag=1;
-        end
- end  
- end
-%     arr;
-%     k=1;
-%     
-%     flag=0
-%     if length(arr)>0
-%         while k<numRaws & flag==0
-%             t=arr
-%             valMin=min(arr)
-%             if valMin~=10000
-%               
-%     %             valMax=max(arr)
-%                  indexMin=find(arr==valMin)
-%     %             indexMax=find(arr==valMax)
-% 
-%     %             arrMin(i)=indexMin;
-% 
-%                  arrMin2(i,k)=indexMin;
-%     %             arrMax(k,i)=find(arr==valMax);
-%                 
-%                 arr(indexMin)=10000;
-%                 k=k+1
-%                 
-%             
-%             else
-%                 flag=1;
-%         end
-%         end
-%     end
         
 
             
